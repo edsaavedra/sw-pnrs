21e234efa1961cc3b8c06410032d8c49a814c535
edsaavedra <eduardosaavedram@live.com>
2020-01-26 20:15:00 -0500

    regular fields

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
 import React, { Component } from 'react';
 import './App.scss';
 
-import Container from '@material-ui/core/Container';
-import Fab from '@material-ui/core/Fab';
-import FormControl from '@material-ui/core/FormControl';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
-import FormLabel from '@material-ui/core/FormLabel';
-import NavigationIcon from '@material-ui/icons/Navigation';
-import Paper from '@material-ui/core/Paper';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import Slider from '@material-ui/core/Slider';
-import Switch from '@material-ui/core/Switch';
-import Typography from '@material-ui/core/Typography';
-
 class App  extends Component {
   constructor(props) {
     super(props);
     this.state = {  }
-  }
-
-  styledRadio(props) {
-    return (
-      <Radio
-        className={'class'}
-        disableRipple
-        color="default"
-        checkedIcon={<span className={'class'} />}
-        icon={<span className={'class'} />}
-        {...props}
-      />
-    );
   }
 
   handleChange() {}
@@ -43,47 +16,47 @@ class App  extends Component {
 
   render() {
     return (
-      <Container className="App">
-        <section component={Paper} elevation={6} className="form">
-          <div className="title">
-          </div>
-          <FormGroup>
-            <div>
-              <FormControl component="fieldset">
-                <FormLabel component="legend">Trip Type</FormLabel>
-                <RadioGroup defaultValue="RoundTrip" aria-label="Trip Type" name="Trip Type">
-                  <FormControlLabel value="RoundTrip" control={<this.styledRadio />} label="RoundTrip" />
-                  <FormControlLabel value="One-way" control={<this.styledRadio />} label="One-way" />
-                  <FormControlLabel value="Multy-city" control={<this.styledRadio />} label="Multy-city" />
-                </RadioGroup>
-              </FormControl>
-            </div>
+      <section className="App">
+        <h1 className="title">Lorem ipsum dolo</h1>
+        <p className="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, quae.</p>
+        <form className="form">
 
-            <div>
-            <Typography id="discrete-slider" gutterBottom>
-              Passengers
-            </Typography>
-            <Slider
-              defaultValue={1}
-              getAriaValueText={this.valuetext}
-              aria-labelledby="discrete-slider"
-              valueLabelDisplay="auto"
-              step={1}
-              marks
-              min={1}
-              max={8}
-            />
-            </div>
-            <div>
-              <FormControlLabel control={<Switch value="checkedC" />} label="Dollars \/ Points" />
-            </div>
-          <Fab variant="extended">
-            <NavigationIcon />
-            Get PNR
-          </Fab>
-          </FormGroup>
-        </section>
-      </Container>
+          <div className="field roundtrip">
+            <span className="title">Trip Type</span>
+            <label for="roundtrip">
+              <input type="radio" name="triptype" value="roundtrip" id="roundtrip"/>
+              <span className="roundtrip">Round Trip</span>
+            </label>
+            <label for="oneway">
+              <input type="radio" name="triptype" value="oneway" id="oneway"/>
+              <span className="oneway">One Way</span>
+            </label>
+            <label for="multicity">
+              <input type="radio" name="triptype" value="multicity" id="multicity"/>
+              <span className="multicity">Multy City</span>
+            </label>
+          </div>
+
+          <div className="field passangers">
+            <label for="passengers">
+              <span className="passengers">passengers</span>
+              <input type="range" min="1" max="8" value="1" id="passengers"/>
+            </label>
+          </div>
+
+          <div className="field payment">
+            <label for="payment">
+              <span className="payment">Dollars</span>
+              <input type="checkbox" value="1" id="payment"/>
+              <span className="payment">Points</span>
+            </label>
+          </div>
+
+          <div className="field"></div>
+
+          <input type="button" className="submit" value="Go"/>
+        </form>
+      </section>
     );
   }
 }
